a3debdd4fa2789bdce0d58a7cb73db2fad747133
asmi046 <asmi046@gmail.com>
2025-08-29 02:27:08 +0300

    Тестовый пользователь админки

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             ContactSeeder::class,
             MenuSeeder::class,
             ParametrSeeder::class,
+            UserSeeder::class,
         ]);
 
         // User::factory(10)->create();
