8ce32a1a92c0192e151ed5eea2b38b276837db11
asmi046 <asmi046@gmail.com>
2025-08-29 12:19:30 +0300

    Мип

---
+++ app/Models/Mips.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mips extends Model
+{
+    protected $fillable = [
+        'title',
+        'cover',
+        'map',
+        'geo',
+        'description',
+        'presentation',
+        'cadastral_number',
+        'address',
+        'area',
+        'urban_potential',
+        'max_floors',
+        'build_percent',
+        'ownership',
+        'land_category',
+        'functional_zone',
+        'usage_type',
+        'special_usage',
+        'encumbrance',
+        'gallery',
+        'social_infrastructure',
+    ];
+}
--- database/migrations/2025_08_28_212644_create_mips_table.php
+++ database/migrations/2025_08_28_212644_create_mips_table.php
@@ -13,6 +13,26 @@ return new class extends Migration
     {
         Schema::create('mips', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->comment('Название проекта');
+            $table->string('cover')->comment('Обложка');
+            $table->string('map')->nullable()->comment('Карта');
+            $table->string('geo')->nullable()->comment('Геопозиция');
+            $table->text('description')->nullable()->comment('Описание проекта');
+            $table->string('presentation')->nullable()->comment('Презентация проекта');
+            $table->string('cadastral_number')->comment('Кадастровый номер');
+            $table->string('address')->comment('Адрес');
+            $table->float('area')->comment('Площадь участка');
+            $table->integer('urban_potential')->comment('Градостроительный потенциал');
+            $table->string('max_floors')->comment('Допустимая этажность');
+            $table->integer('build_percent')->comment('Процент застройки');
+            $table->string('ownership')->comment('Собственность');
+            $table->string('land_category')->nullable()->comment('Категория земель');
+            $table->string('functional_zone')->nullable()->comment('Функциональная зона');
+            $table->text('usage_type')->nullable()->comment('Вид использования');
+            $table->text('special_usage')->nullable()->comment('Особые условия использования');
+            $table->string('encumbrance')->nullable()->comment('Обременение');
+            $table->json('gallery')->nullable()->comment('Галерея');
+            $table->json('social_infrastructure')->nullable()->comment('Социальная инфраструктура');
             $table->timestamps();
         });
     }
